9a12b623affbf723d9489b01a9232bfba6e626fb
r-zane-spalding <zane.spalding@gmail.com>
2022-04-22 23:46:02 -0400

    Making things look better

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
+import React from 'react'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import Wrapper from '../components/Wrapper'
+import Head from 'next/head'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
+  <>
+    <style jsx global>{` body{ margin: 0px; padding: 0px; background: #D3D3D3 } `}</style>
+    <Wrapper>
+      <div style={{width: '100%', height: '100vh', background: 'red'}}></div>
+      <div style={{width: '100%', height: '100vh', background: 'blue'}}></div>
+      <div style={{width: '100%', height: '100vh', background: 'green'}}></div>
+    </Wrapper>
+  </>
 )
 
 export default IndexPage
+
+
+
+
+
+
+
+{/* <Layout title="Home | Next.js + TypeScript Example">
+  //   <h1>Hello Next.js 👋</h1>
+  //   <p>
+  //     <Link href="/about">
+  //       <a>About</a>
+  //     </Link>
+  //   </p>
+  // </Layout> */}
